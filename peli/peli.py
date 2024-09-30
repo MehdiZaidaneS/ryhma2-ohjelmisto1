6e388abf0461cde1eab56b5286eaeb3aa89c7dfb
Aleksipc <aleklap@metropolia.figit config --global user.emailgit config --global user.email aleklap@metropolia.fi>
2024-09-30 21:02:43 +0300

    yks funktio

--- peli/peli.py
+++ peli/peli.py
@@ -1,8 +1,36 @@
 import mysql.connector
 from geopy.distance import distance
+import math
+import random
+
+
+def randommaa():
+    #arpoo etsittävän maan
+    maat = (f"select name from country;")
+    kursori = yhteys.cursor()
+    kursori.execute(maat)
+    maannimet = kursori.fetchall()
+    #maannimet on nyt lista tupleja, en tiedä miksi joku sql juttu
+    #lasketaan listan objektien määrä, arvotaan random luku tämän määrän sisällä
+    #valitaan random luvun kohdalla oleva objekti maaksi
+    #sitten vielä siistitään tuple stringiksi
+    
+    objektienmaara = len(maannimet)
+
+    randomvalinta = random.randint(0,objektienmaara-1)
+
+    valittumaa = maannimet[randomvalinta]
+
+    #maan nimi oli aina tuplen ensimmäisessä indexissä:
+    siistittyvalittumaa = valittumaa[0]
+
+    return siistittyvalittumaa
+    
 
 def maanlentokenttienlokaatioidenkeskiarvo(maannimi):
-    #Esimerkki
+    #funktio etsii tietokannasta kaikki parametriksi annetun maan lentokentät ja
+    #laskee niiden lokaatioiden keskiarvon ja palauttaa tämän arvon 
+
     listalokaatioista = (f"select latitude_deg, longitude_deg from airport, country where airport.iso_country = country.iso_country and country.name = '{maannimi}';")
     kursori = yhteys.cursor()
     kursori.execute(listalokaatioista)
@@ -26,15 +54,29 @@ def maanlentokenttienlokaatioidenkeskiarvo(maannimi):
     return (latitude_keskiarvo, longitude_keskiarvo)
 
 
+def suunta(arvattumaa,tavoitemaa):
+    #funktio suunta saa parametreiksi arvatun maan lokaation ja oikean maan lokaation
+    #laskee maiden välisen kulman olettaen että pohjoinen on 0/360 etelä 180 jne
+    #palauttaa kulman arvon
+    
+
+
+    return 0
+
+
 
 yhteys = mysql.connector.connect(
     host="localhost",
     port= 3306,
     database="flight_game",
     user="user2",
-    password="salasana",
+    password="1234",
     autocommit=True
 )
 
+
+
 x = maanlentokenttienlokaatioidenkeskiarvo("Italy")
 print(x)
+
+print(randommaa())
